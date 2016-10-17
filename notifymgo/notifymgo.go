3425489670915bac241afa2c41c353812f05cd0e
Centny <centny@gmail.com>
2016-10-17 16:32:57 +0800

    adding notify mgo imply.

---
+++ notifymgo/notifymgo.go
@@ -0,0 +1,73 @@
+package notifymgo
+
+import (
+	"github.com/Centny/gwf/netw/rc/plugin"
+	"github.com/Centny/gwf/util"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//NotifyMgo is impl to NotifyDb by mongo
+type NotifyMgo struct {
+	Name  string
+	C     func(name string) *mgo.Collection
+	Count map[string]int
+}
+
+//NewNotifyMgo is NotifyMgo creator
+func NewNotifyMgo(c func(name string) *mgo.Collection) *NotifyMgo {
+	return &NotifyMgo{
+		Name:  "rc_notify",
+		C:     c,
+		Count: map[string]int{},
+	}
+}
+
+//AddMessage @see NotifyDb
+func (n *NotifyMgo) AddMessage(m *plugin.Message) error {
+	m.ID = bson.NewObjectId().Hex()
+	m.Time = util.Now()
+	return n.C(n.Name).Insert(m)
+}
+
+//RemoveMessage @see NotifyDb
+func (n *NotifyMgo) RemoveMessage(id string) error {
+	return n.C(n.Name).RemoveId(id)
+}
+
+//DoneMessage @see NotifyDb
+func (n *NotifyMgo) DoneMessage(mid, key string) (msg *plugin.Message, err error) {
+	msg = &plugin.Message{}
+	_, err = n.C(n.Name).Find(bson.M{
+		"_id": mid,
+		"marked": bson.M{
+			"$ne": key,
+		},
+	}).Apply(mgo.Change{
+		Upsert:    false,
+		ReturnNew: true,
+		Update: bson.M{
+			"$addToSet": bson.M{
+				"marked": key,
+			},
+			"$inc": bson.M{
+				"count": 1,
+			},
+		},
+	}, msg)
+	return
+}
+
+//RemoveCount @see NotifyDb
+func (n *NotifyMgo) RemoveCount(mtype string) (count int, err error) {
+	if val, ok := n.Count[mtype]; ok {
+		return val, nil
+	}
+	return 1, nil
+}
+
+//ListMessage @see NotifyDb
+func (n *NotifyMgo) ListMessage(m *plugin.Message) (ms []*plugin.Message, err error) {
+	err = n.C(n.Name).Find(bson.M{"type": m.Type}).All(&ms)
+	return
+}
---
+++ notifymgo/notifymgo_test.go
@@ -0,0 +1,89 @@
+package notifymgo
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Centny/dbm/mgo"
+	"github.com/Centny/gwf/netw/rc/plugin"
+	"github.com/Centny/gwf/netw/rc/rctest"
+)
+
+func TestNotifyMgo(t *testing.T) {
+	mgo.AddDefault2("cny:123@loc.w:27017/cny")
+	mgo.C("rc_notify").RemoveAll(nil)
+	plugin.ShowLog = 1
+	var err error
+	var rct *rctest.RCTest
+	var mdb *NotifyMgo
+	var srv *plugin.NotifySrv
+	var client *plugin.NotifyClient
+	var received = 0
+
+	//
+	//test normal post
+	{
+		received = 0
+		rct = rctest.NewRCTest_j2(":9332")
+		mdb = NewNotifyMgo(mgo.C)
+		srv = plugin.NewNotifySrv(mdb)
+		srv.Hand(rct.L)
+		srv.Start()
+		//
+		client = plugin.NewNotifyClient(plugin.NotifyHandlerF(func(n *plugin.NotifyClient, m *plugin.Message) error {
+			if len(m.ID) < 1 {
+				panic("xxx")
+			}
+			if m.Type == "testing" || m.Type == "xx" {
+				var err = n.Mark(m.ID, "testing")
+				if err != nil {
+					t.Error(err)
+					return err
+				}
+				received++
+				return err
+			}
+			return nil
+		}))
+		client.SetRunner(rct.R, rct.Rmh)
+		//
+		err = client.Monitor("testing,xx", 10)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		for i := 0; i < 10; i++ {
+			err = srv.PostMessage(&plugin.Message{
+				Type: "testing",
+			})
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			err = srv.PostMessage(&plugin.Message{
+				Type: "xx",
+			})
+			if err != nil {
+				t.Error(err)
+				return
+			}
+		}
+		for x := 0; x < 10 && received < 20; x++ {
+			time.Sleep(100 * time.Millisecond)
+		}
+		if received != 20 {
+			t.Error("not received")
+			return
+		}
+		if cc, _ := mgo.C(mdb.Name).Count(); cc > 0 {
+			t.Error("error")
+			return
+		}
+		srv.Stop()
+		rct.R.Stop()
+		rct.L.Close()
+		rct.L.Wait()
+		fmt.Printf("\n\n\n")
+	}
+}
